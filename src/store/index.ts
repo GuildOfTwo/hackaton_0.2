80f24ed5bc296b0f8f687db3872d6b4efe406aca
Alexander Berezin <zx27111@yandex.ru>
2023-11-03 17:11:41 +0300

    feat-store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,10 @@
-export {}
+import { configureStore } from "@reduxjs/toolkit"
+import rootReducer from './redusers/index'
+
+export type RootState = ReturnType<typeof rootReducer>
+
+const store = configureStore({
+    reducer: rootReducer
+})
+
+export default store
---
+++ src/utils/types/types.ts
@@ -0,0 +1,11 @@
+export type userSliseState ={
+    user: null | TUser,
+    error: string,
+    isLoading: boolean,
+    isAuth: boolean,
+}
+
+export type TUser = {
+    name: string,
+    email: string
+}
