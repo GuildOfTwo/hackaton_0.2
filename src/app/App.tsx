cc9c53db3288dc076e7e620da0af962266ec4723
Vlad Belousov <belousov7vlad@gmail.com>
2023-11-04 16:57:08 +0300

    refactor: routes uri changed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import AppRoutes from '../routes';
 import { Modal } from '../components/modal';
 import { AppStyles } from './styled';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const App: React.FC = () => {
+  const location = useLocation()
+const navigate = useNavigate()
+  React.useEffect(() => {
+    location.pathname === '/' ? navigate('dashboard') : ''
+  }, [])
   return (
     <>
       <AppStyles />
--- src/utils/constants/navigation/index.ts
+++ src/utils/constants/navigation/index.ts
@@ -3,13 +3,13 @@ export const MAIN_PAGE_URI = '/';
 export const LOGIN_PAGE_URI = 'login';
 export const UNAVAILABLE_PAGE_URI = 'unavailable';
 export const NOT_FOUND_URI = '*';
-export const DASHBOARD = 'dashboard'
-export const ALL_COURSES = 'all-courses'
+export const DASHBOARD_URI = 'dashboard'
+export const ALL_COURSES_URI = 'all-courses'
 export const Page = {
   MAIN: MAIN_PAGE_URI,
   LOGIN: LOGIN_PAGE_URI,
   UNAVAILABLE: UNAVAILABLE_PAGE_URI,
   NOT_FOUND: NOT_FOUND_URI,
-  DASHBOARD: DASHBOARD,
-  ALL_COURSES: ALL_COURSES
+  DASHBOARD: DASHBOARD_URI,
+  ALL_COURSES: ALL_COURSES_URI
 };
