6bfb1d0ca5a956d01414d554a3549e7e55d5c7f3
Vlad Belousov <belousov7vlad@gmail.com>
2023-11-04 15:17:24 +0300

    style: max-width removed

--- src/app/styled.ts
+++ src/app/styled.ts
@@ -3,7 +3,7 @@ import { Layout } from "antd";
 import { Content } from "antd/es/layout/layout";
 
 const AppWrapper = styled(Layout)`
-  max-width: 1920px;
+  /* max-width: 1920px; */
   display: flex;
   flex-direction: column;
   margin-right: auto;
