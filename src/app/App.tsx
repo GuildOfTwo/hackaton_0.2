37518390289f0efefc4a8561aad1269bb8eb5d99
Vlad Belousov <belousov7vlad@gmail.com>
2023-11-04 10:34:07 +0300

    fix: comments removed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { Layout } from 'antd';
-import Header from '../layouts/Header';
-import Main from '../layouts/Main';
 import AppRoutes from '../routes';
-import { AppWrapper, ContentWrapper, Normalize } from './styled';
+import { AppWrapper, Normalize } from './styled';
 
 const App: React.FC = () => {
-  // const { Content, Footer } = Layout;
   return (
     <AppWrapper >
       <Normalize />
       <AppRoutes />
-      {/* <Header />
-      <ContentWrapper >
-        <Main isSider={true}>
-          <Content >
-            <AppRoutes />
-          </Content>
-        </Main>
-      </ContentWrapper>
-      <Footer /> */}
     </AppWrapper>
   );
 };
