de311b7e1f4fa8aaebeb09f4c11107210b23f81e
Vlad Belousov <belousov7vlad@gmail.com>
2023-11-04 21:26:19 +0300

    feat: submenu added

--- src/layouts/Sider/index.tsx
+++ src/layouts/Sider/index.tsx
@@ -1,49 +1,58 @@
-import { Menu } from 'antd';
-import { UserOutlined, VideoCameraOutlined, UploadOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Menu, MenuProps } from 'antd';
+import {
+  UserOutlined, VideoCameraOutlined, UploadOutlined, MenuFoldOutlined, MenuUnfoldOutlined, PieChartOutlined, DesktopOutlined,
+  TeamOutlined, FileOutlined
+} from '@ant-design/icons';
 import Sider from 'antd/es/layout/Sider';
 import { useState } from 'react';
 import { HEADER_HEIGHT } from '../../utils/constants';
 import { Link } from 'react-router-dom';
-import { ALL_COURSES_URI } from '../../utils/constants/navigation';
 import { SliderButton } from './styled';
 import { Page } from '../../utils/constants/navigation';
+
+type MenuItem = Required<MenuProps>['items'][number];
 
 
 const Slider = () => {
   const [collapsed, setCollapsed] = useState(false);
+
+  const items: MenuItem[] = [
+    getItem(<Link to={Page.DASHBOARD}>Dashboard</Link>, '1', <PieChartOutlined />),
+    getItem(<Link to={Page.ALL_COURSES}>Все курсы</Link>, '2', <DesktopOutlined />, [
+      getItem(<Link to={Page.SELECTED_COURSES}>Менеджмент</Link>, '3'),
+    ]),
+    getItem(<Link to={Page.SELECTED_COURSES}>Назначенные курсы</Link>, 'sub1', <UserOutlined />)
+  ];
+  function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+  ): MenuItem {
+    return {
+      key,
+      icon,
+      children,
+      label,
+    } as MenuItem;
+  }
   return (
     <div style={{ position: "absolute", height: "calc(100% - 66px)", zIndex: '900' }}>
       <Sider trigger={null} collapsible collapsed={collapsed} style={{ background: 'transparent', margin: '66px 0 0', height: '100%' }}>
         <div className="demo-logo-vertical" />
         <Menu
           theme="light"
+          defaultSelectedKeys={["1"]}
           mode="inline"
+          items={items}
           style={{ height: `calc(100vh - ${HEADER_HEIGHT})` }}
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: <Link to={Page.DASHBOARD}>Dashboard</Link>,
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: <Link to={ALL_COURSES_URI}>Назначенные курсы</Link>
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: <Link to={ALL_COURSES_URI}>Назначенные курсы</Link>
-            },
-          ]}
         />
       </Sider>
       <SliderButton
         type="text"
         icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
         onClick={() => setCollapsed(!collapsed)}
-       /></div>
+      /></div>
   )
 }
 
--- src/utils/constants/navigation/index.ts
+++ src/utils/constants/navigation/index.ts
@@ -6,12 +6,22 @@ export const NOT_FOUND_URI = '*';
 export const DASHBOARD_URI = 'dashboard'
 export const ALL_COURSES_URI = 'all-courses'
 export const AUTHORIZATION_URI = 'authorization'
+export const SELECTED_COURSES_URI = 'selected-courses'
+
+export const MANAGMENT_COURSE_URI = 'managment'
 export const Page = {
   MAIN: MAIN_PAGE_URI,
   LOGIN: LOGIN_PAGE_URI,
   UNAVAILABLE: UNAVAILABLE_PAGE_URI,
   NOT_FOUND: NOT_FOUND_URI,
   ALL_COURSES: ALL_COURSES_URI,
+  SELECTED_COURSES: SELECTED_COURSES_URI,
   DASHBOARD: DASHBOARD_URI,
-  AUTHORIZATION : AUTHORIZATION_URI
+  AUTHORIZATION: AUTHORIZATION_URI
 };
+
+
+export const SubPage = {
+  MANAGMENT: MANAGMENT_COURSE_URI
+
+}
