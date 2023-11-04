f28570afd8e90658ec5aad968780de344d5ae2ae
Vlad Belousov <belousov7vlad@gmail.com>
2023-11-04 16:57:46 +0300

    fix: depend added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
 const navigate = useNavigate()
   React.useEffect(() => {
     location.pathname === '/' ? navigate('dashboard') : ''
-  }, [])
+  }, [location])
   return (
     <>
       <AppStyles />
