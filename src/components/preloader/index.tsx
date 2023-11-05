7c57f7f1be0c3a899ce53f7434ff2a8dab4aa883
Alexander Berezin <zx27111@yandex.ru>
2023-11-05 11:35:13 +0300

    feat-preloader

---
+++ src/components/preloader/index.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+ const Loader = styled.div`
+  width: 48px;
+  height: 48px;
+  border: 2px solid #5c0da7;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+
+
+  &:after {
+    content: '';  
+  box-sizing: border-box;
+  position: absolute;
+  left: 50%;
+  top: 0;
+  background: #FF3D00;
+  width: 3px;
+  height: 24px;
+  transform: translateX(-50%);
+  }
+
+  @keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }}
+`;
+
+
+
+export const Preloader: FC = () => {
+
+    return (
+        <Container>
+            <Loader />
+        </Container>
+    );
+
+}
