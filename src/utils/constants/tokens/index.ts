685b7cc511455c0578735f7d381f3bb27cc8e085
Vlad Belousov <belousov7vlad@gmail.com>
2023-11-05 10:34:03 +0300

    feat: bot api added

---
+++ src/utils/constants/tokens/index.ts
@@ -0,0 +1,5 @@
+export const TELEGRAM_TOKEN = "6776594984:AAG9CpZvT1fSB5Ds3LbqWWBCY_X09-QRg88"
+
+
+
+export const TELEGRAM_BOT_SEND_MESSAGE_URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
