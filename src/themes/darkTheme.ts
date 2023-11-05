25109af1ef86b9e8978bb68d2fcffb08662f2617
Kirill Chugunov <chugunov@ferrata.pro>
2023-11-06 01:40:37 +0300

    refactor-theme-swtich-files

---
+++ src/themes/darkTheme.ts
@@ -0,0 +1,8 @@
+const darkTheme = {
+  textcolor: '#D2D2D2',
+  backgroundColor1: '#0A1F32',
+  backgroundColor2: '#2D384A',
+  backgroundColor3: '#344756'
+}
+
+export default darkTheme;
---
+++ src/themes/defaultTheme.ts
@@ -0,0 +1,9 @@
+
+const defaultTheme = {
+  textcolor: 'black',
+  backgroundColor1: '#ebf1f1',
+  backgroundColor2: '#bec3c6',
+  backgroundColor3: '#7C7C7C'
+}
+
+export default defaultTheme;
