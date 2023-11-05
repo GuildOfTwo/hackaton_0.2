f5598f918bd5c908177e097c3e8eb2a752d225bb
Kirill Chugunov <chugunov@ferrata.pro>
2023-11-06 01:38:34 +0300

    refactor-theme-switch

--- src/components/Theme/Theme.tsx
+++ src/components/Theme/Theme.tsx
@@ -2,21 +2,8 @@ import { ThemeProvider } from "styled-components";
 import { TThemeProps } from "../../utils/types/types";
 import { useState, useEffect } from 'react';
 import { useAppSelector } from "../../hooks";
-
-
-const defaultTheme = {
-  textcolor: 'black',
-  backgroundColor1: '#ebf1f1',
-  backgroundColor2: '#bec3c6',
-  backgroundColor3: '#7C7C7C'
-}
-
-const darkTheme = {
-  textcolor: '#D2D2D2',
-  backgroundColor1: '#0A1F32',
-  backgroundColor2: '#2D384A',
-  backgroundColor3: '#344756'
-}
+import darkTheme from "../../themes/darkTheme";
+import { defaultTheme } from "antd/es/theme/context";
 
 const Theme = ({ children }: TThemeProps) => {
   const currentTheme = useAppSelector(store => store.theme.theme)
