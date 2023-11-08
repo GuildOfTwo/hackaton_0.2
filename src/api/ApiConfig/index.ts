f8affbec915e580459000882e6aa39c84a6f0acc
Vlad Belousov <belousov7vlad@gmail.com>
2023-11-08 13:00:52 +0300

    fix: cookies fix

--- src/api/ApiConfig/index.ts
+++ src/api/ApiConfig/index.ts
@@ -3,7 +3,7 @@ import { BACKEND_URL } from '../../utils/constants/api';
 
 export const backendInstance = axios.create({
   // baseURL:BACKEND_URL,
-  baseURL: `https://cors-anywhere.herokuapp.com/${BACKEND_URL}`,
+  baseURL: `${BACKEND_URL}`,
   headers: {
     // 'Content-Type': 'multipart/form-data',
     // 'X-ACCESS-TOKEN': REACT_APP_BACKEND_API_TOKEN
--- src/api/authorization/index.ts
+++ src/api/authorization/index.ts
@@ -1,9 +1,23 @@
-import { backendInstance } from '../ApiConfig';
+import { BACKEND_URL } from '../../utils/constants/api';
+// import { backendInstance } from '../ApiConfig';
+// import { backendInstance } from '../ApiConfig';
 
 export const loginRequest = (email: string, password: string) => {
   console.log(email, password);
-  return backendInstance.post('/signin', {
-    email: email,
-    password: password,
-  });
+
+  return fetch(`${BACKEND_URL}signin`, {
+    method: 'POST',
+    // credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify({ email: email, password: password }),
+  })
+    .then((response) => {
+      response.headers.getSetCookie();
+    })
+    .catch((error) => {
+      console.error('Request failed', error);
+    });
 };
