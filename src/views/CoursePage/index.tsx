6a1546f3eec3d7cbf18c95b0ca1c619f158b0c80
Vlad Belousov <belousov7vlad@gmail.com>
2023-11-09 18:57:32 +0300

    fix

--- src/views/CoursePage/index.tsx
+++ src/views/CoursePage/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { CourseContainer } from './style'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react';
@@ -8,32 +7,27 @@ import { TSelectCourse } from '../../utils/types/types';
 
 export const CoursePage = () => {
 
-   const {id} = useParams()
+  const { id } = useParams()
 
-   console.log(id);
-   
-   const [course, setCourse] = useState<TSelectCourse | null>()
+  const [course, setCourse] = useState<TSelectCourse | null>()
 
-   useEffect(() => {
+  useEffect(() => {
     requestSoloCourses(id).then((res) => setCourse(res.data))
-   }, [])
+  }, [])
 
 
- 
-   if (!course) return null
 
- const content = JSON.parse(course?.CourseContent[0]?.content)
+  if (!course) return null
+
+  const content = JSON.parse(course?.CourseContent[0]?.content)
 
 
-console.log(content);
- 
-return (
+  return (
 
     <CourseContainer>
-        <p>{course.courseName}</p>
-        <Markdown>123</Markdown>
+      <p>{course.courseName}</p>
+      <Markdown>{content.text ? content.text : ''}</Markdown>
     </CourseContainer>
 
   )
 }
-
